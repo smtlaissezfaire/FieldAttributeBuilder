0be626e6210f8a662dc5d89cbe9b9ca35b721bd0
Scott Taylor <scott@railsnewbie.com>
2009-04-28 19:33:05 -0400

    Add VERSION # spec

--- spec/field_attribute_builder_spec.rb
+++ spec/field_attribute_builder_spec.rb
@@ -1,5 +1,9 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe FieldAttributeBuilder do
-  
+  describe "Version" do
+    it "should be at version 0.0.1" do
+      FieldAttributeBuilder::VERSION.should == "0.0.1"
+    end
+  end
 end
